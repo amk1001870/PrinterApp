d6cf7f7da71d91dc76f6f973a87a3429152d5962
amk1001870 <amk1001870@student.hamk.fi>
2024-10-20 13:58:59 +0300

    declearing variables called creator

---
+++ printerApp.java
@@ -0,0 +1,11 @@
+/**
+ * printerApp
+ */
+public class printerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Printer App");
+        String creator;
+        
+    }
+}
