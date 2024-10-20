e3e9872c87de368a2a6198e9e6bc21b52e91b896
amk1001870 <amk1001870@student.hamk.fi>
2024-10-20 14:24:08 +0300

    moving all declerations of the variables of the top to make code a bit clear

--- printerApp.java
+++ printerApp.java
@@ -4,29 +4,29 @@
 public class printerApp {
 
     public static void main(String[] args) {
-        System.out.println("Printer App");
         String creator;
-        creator="pravesh bahakari";
-        System.out.println("This app is made by: " +creator);
         int number1;
         int number2;
+        int multiplication;
+        int addition;
+        int substraction;
+        double division;
+        System.out.println("Printer App");
+        creator="pravesh bahakari";
+        System.out.println("This app is made by: " +creator);
        number1=8;
        number2=7;
      // System.out.println(number1 );
        System.out.println("The value of number1 is : "+number1);
        System.out.println("The value of number2 is : "+number2);
-       int multiplication;
-       multiplication=number1*number2;
-       System.out.println(number1+" * "+number2+" = "+multiplication);
-       int addition;
-       addition=number1+number2;
-       System.out.println(number1+" + "+number2+" = "+addition);
-       int substraction;
-       substraction=number1-number2;
-       System.out.println(number1+" - "+number2+" = "+substraction);
-       double division;
-       division=number1/number2;
-       System.out.println(number1+" / "+number2+" = "+division);
+      multiplication=number1*number2;
+      System.out.println(number1+" * "+number2+" = "+multiplication);
+      addition=number1+number2;
+      System.out.println(number1+" + "+number2+" = "+addition);
+      substraction=number1-number2;
+      System.out.println(number1+" - "+number2+" = "+substraction);
+      division=number1/number2;
+      System.out.println(number1+" / "+number2+" = "+division);
        
 
 
