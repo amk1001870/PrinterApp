1e8cd2b76a2dca7927a0e6183c077212fc2599d9
amk1001870 <amk1001870@student.hamk.fi>
2024-10-20 14:01:53 +0300

    declearing int type variables number1 and number2

--- printerApp.java
+++ printerApp.java
@@ -6,6 +6,12 @@ public class printerApp {
     public static void main(String[] args) {
         System.out.println("Printer App");
         String creator;
+        creator="pravesh bahakari";
+        System.out.println("This app is made by: "+creator);
+        int number1;
+        int numbber2;
         
+
+
     }
 }
